095950f77aa7477f86bb1355fe0a2a3d7166821f
fujiabao <1181488349@qq.com>
2019-12-12 17:49:40 +0800

    项目模型

--- web/web/settings/base.py
+++ web/web/settings/base.py
@@ -28,6 +28,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
